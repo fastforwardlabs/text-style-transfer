6bb856a0d9425ef664897459d6c4b46ddb854485
Andrew Reed <areed@cloudera.com>
2022-02-25 19:27:10 +0000

    add setup.py and install

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="text-style-transfer",
+    version="0.0.1",
+    description="A repo for experimenting with text style transfer.",
+    author="Andrew Reed, Daniel Valdez Balderas, Melanie Beck",
+)
