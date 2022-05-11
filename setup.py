77e0d255b6f74675bf5647c410f2a420c6ad8f20
Andrew Reed <areed@cloudera.com>
2022-05-11 16:51:03 +0000

    cleanup files

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name="text-style-transfer",
     version="0.0.1",
     description="A repo for experimenting with text style transfer.",
-    author="Andrew Reed, Daniel Valdez Balderas, Melanie Beck",
+    author="Andrew Reed, Melanie Beck",
 )
