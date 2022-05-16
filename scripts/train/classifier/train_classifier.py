f647e4dd57c6363f40e5626c67e84a9c823000f6
Andrew Reed <areed@cloudera.com>
2022-05-16 18:59:43 +0000

    implement flag to turn of data shuffle at train time

--- scripts/train/classifier/train_classifier.py
+++ scripts/train/classifier/train_classifier.py
@@ -5,6 +5,7 @@ from typing import Optional
 
 
 from collections import defaultdict
+from torch.utils.data import SequentialSampler, BatchSampler, DataLoader
 from datasets import (
     load_dataset,
     load_from_disk,
@@ -98,8 +99,70 @@ class MiscArguments:
     model_name_or_path: str = field(
         metadata={"help": "Path to pretrained model or model identifier from huggingface.co/models"}
     )
+    dataset_name: Optional[str] = field(
+        default=None,
+        metadata={"help": "The name of the dataset to use (via the datasets library)."},
+    )
+    shuffle_train: bool = field(
+        default=True,
+        metadata={"help": "When set to False, no shuffling is done at train time"}
+    )
 
 
+class CustomTrainer(Trainer):
+    """
+    A custom Trainer that overwrites and subclasses the `get_train_dataloader()` method.
+
+    This customization allows us to introduce a flag that disables shuffling on the DataLoader. When
+    `shuffle_train` flag is True, a RandomSampler is used via `self._get_train_sampler`. When set to False,
+    a BatchSampler(SequentialSampler()) is utilized in the dataloader.
+
+    """
+
+    def __init__(self, shuffle_train, *args, **kwargs):
+        self.shuffle_train = shuffle_train
+        super().__init__(*args, **kwargs)
+
+    def seed_worker(self, _):
+        """
+        Helper function to set worker seed during Dataloader initialization.
+        """
+        worker_seed = torch.initial_seed() % 2**32
+        set_seed(worker_seed)
+
+    def get_train_dataloader(self) -> DataLoader:
+        if self.train_dataset is None:
+            raise ValueError("Trainer: training requires a train_dataset.")
+
+        train_dataset = self.train_dataset
+        data_collator = self.data_collator
+
+        if isinstance(train_dataset, Dataset):
+            train_dataset = self._remove_unused_columns(
+                train_dataset, description="training"
+            )
+        else:
+            data_collator = self._get_collator_with_removed_columns(
+                data_collator, description="training"
+            )
+
+        if self.shuffle_train:
+            train_sampler = self._get_train_sampler()
+        else:
+            train_sampler = SequentialSampler(self.train_dataset)
+
+        return DataLoader(
+            train_dataset,
+            batch_size=self.args.per_device_train_batch_size,
+            sampler=train_sampler,
+            collate_fn=self.data_collator,
+            drop_last=self.args.dataloader_drop_last,
+            num_workers=self.args.dataloader_num_workers,
+            pin_memory=self.args.dataloader_pin_memory,
+            worker_init_fn=self.seed_worker,
+        )
+    
+    
 def main():
     
     set_seed(42)
@@ -111,12 +174,13 @@ def main():
     
     if not os.path.exists(training_args.output_dir):
         os.makedirs(training_args.output_dir)
-        
-    print("TEST!!!!", misc_args.model_name_or_path)
     
     # load WNC classification dataset
-    CLS_DATASET_PATH = "/home/cdsw/data/processed/WNC_full_cls"
-    wnc_full_cls = load_from_disk(CLS_DATASET_PATH)
+    if data_args.dataset_name in ['wnc_cls_full']:
+        CLS_DATASET_PATH = f"data/processed/WNC_cls{'_'.join(data_args.dataset_name.split('_')[2:])}"
+        datasets = load_from_disk(CLS_DATASET_PATH)
+    else:
+        raise ValueError("Must specify the classification version of WNC: wnc_cls_full")
     
     # load base-model and tokenizer
     checkpoint = misc_args.model_name_or_path
@@ -125,7 +189,7 @@ def main():
     def tokenize_function(example):
         return tokenizer(example["text"], truncation=True)
 
-    tokenized_datasets = wnc_full_cls.map(tokenize_function, batched=True)
+    tokenized_datasets = datasets.map(tokenize_function, batched=True)
     data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
 
@@ -144,8 +208,9 @@ def main():
             "f1": f1_metric.compute(predictions=predictions, references=labels),
         }
 
-    trainer = Trainer(
-        model,
+    trainer = CustomTrainer(
+        shuffle_train=misc_args.shuffle_train,
+        model=model,
         args=training_args,
         train_dataset=tokenized_datasets["train"],
         eval_dataset=tokenized_datasets["validation"],
